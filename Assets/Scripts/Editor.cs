9fcc23c6a6a3c7faff278fcdb4ae259a5ff6f36e
240898584 <240898584@qq.com>
2025-06-22 21:51:07 +0800

    更改

--- Assets/Scripts/Editor.cs
+++ Assets/Scripts/Editor.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class Editor : MonoBehaviour
+public class Editor : EditorWindow
 {
     // Start is called before the first frame update
     void Start()
